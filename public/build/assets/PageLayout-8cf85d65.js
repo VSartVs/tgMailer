6715c12fd371496297c33ef9b2ec569c20fbb6dc
VSartVs <tripolskaya.v@internet.ru>
2023-08-21 00:40:37 +0300

    build

---
+++ public/build/assets/PageLayout-8cf85d65.js
@@ -0,0 +1,1 @@
+import{_ as o,r,c as t,o as a}from"./app-0bc45747.js";const s={};function c(n,_){const e=r("router-view");return a(),t(e)}const i=o(s,[["render",c],["__file","C:/OSPanel/domains/tgMailer/resources/js/layouts/PageLayout.vue"]]);export{i as default};
